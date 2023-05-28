8b444eee9a913f24dcd3ad2bd0ce9c0b5b7010d7
yoonsoodev <yoonsoo98@ewhain.net>
2023-05-28 14:01:51 +0900

    설정 변경 및 parser.y 추가

---
+++ parser_tab.h
@@ -0,0 +1,30 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	tident	258
+#define	tnumber	259
+#define	tconst	260
+#define	telse	261
+#define	tif	262
+#define	teif	263
+#define	tint	264
+#define	treturn	265
+#define	tvoid	266
+#define	twhile	267
+#define	taddAssign	268
+#define	tsubAssign	269
+#define	tmulAssign	270
+#define	tdivAssign	271
+#define	tmodAssign	272
+#define	tor	273
+#define	tand	274
+#define	tequal	275
+#define	tnotequ	276
+#define	tgreate	277
+#define	tlesse	278
+#define	tinc	279
+#define	tdec	280
+#define	LOWER_THAN_ELSE	281
+
+
+extern YYSTYPE yylval;
